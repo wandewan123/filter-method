ce9f27e534ad82497c7f86bda44fb51f226cbada
Wandewan <dewanjava321@gmail.com>
2026-05-08 23:54:16 +0700

    Update core.py

--- dvipy/core.py
+++ dvipy/core.py
@@ -1,1 +1,18 @@
+import numpy as np
+# Kita asumsikan nanti hasil kompilasi f2py bernama 'solver_lib'
+try:
+    import solver_lib
+except ImportError:
+    solver_lib = None
 
+def tridag(a, b, c, r):
+    """
+    Wrapper untuk memanggil subroutine tridag dari Fortran.
+    """
+    if solver_lib is None:
+        raise ImportError("Modul Fortran 'solver_lib' belum dikompilasi. Jalankan f2py dulu.")
+    
+    n = len(r)
+    # Memanggil subroutine dari module 'solver_module' yang ada di Fortran
+    u = solver_lib.solver_module.tridag(a, b, c, r, n)
+    return u
