0b46c2c3d3bdc5985efbc2fed9938b5ee49393a1
Wandewan <dewanjava321@gmail.com>
2026-05-10 05:15:17 +0700

    Update core.py

--- dvipy/core.py
+++ dvipy/core.py
@@ -1,20 +1,20 @@
 import numpy as np
-try:
-    from . import solver_lib
-except ImportError:
-    solver_lib = None
+from . import solver_lib
 
 def tridag(a, b, c, r):
-    n = len(r)
-    return solver_lib.solver_module.tridag(a, b, c, r, n)
+    return solver_lib.solver_module.tridag(a, b, c, r)
 
 def psi_eigen(dx, vpot, psi, energy):
-    # n otomatis diurus f2py jika intent(in) diatur benar
-    return solver_lib.solver_module.psi_eigen_fortran(dx, vpot, psi, energy, len(vpot))
+    return solver_lib.solver_module.psi_eigen_fortran(
+        dx, vpot, psi, energy
+    )
 
 def energi_eigen(dx, vpot, psi):
-    return solver_lib.solver_module.energi_eigen_fortran(dx, vpot, psi, len(vpot))
+    return solver_lib.solver_module.energi_eigen_fortran(
+        dx, vpot, psi
+    )
 
 def solve_full(x, vpot, initial_e, dx, xi=0.95, tol=1e-7):
-    # Ini fungsi "all-in-one" kencang yang kamu mau
-    return solver_lib.solver_module.main_filter(x, len(x), vpot, initial_e, dx, xi, tol)
+    return solver_lib.solver_module.main_filter(
+        x, vpot, initial_e, dx, xi, tol
+    )
