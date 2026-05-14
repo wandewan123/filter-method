61df77b189dd0913185677c5835a8f4b46183e15
Dewan Tamsih <dewanjava321@gmail.com>
2026-05-15 00:44:55 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
-# Hapus 'from setuptools import setup' yang berlebihan
 from numpy.distutils.core import Extension, setup
 
-# Menghubungkan ke file Fortran di folder src
-# Urutan file sangat penting: Modul dasar didahulukan!
 ext = Extension(
-    name='dvipy.solver_lib', # <-- Ditambahkan awalan 'dvipy.'
-    sources=[
-        'src/matrix.f90',    # Asumsi: ini modul dasar tidak memanggil yang lain
-        'src/eigene.f90',    # Asumsi: modul dasar
-        'src/eigenf.f90',    # Asumsi: modul dasar
-        'src/main.f90',      # Memanggil matrix, eigene, eigenf
-        'src/solver.f90'     # (Jika ini adalah file wrapper utamanya)
+    name       =    'dvipy.solver_lib', 
+    sources    =    [
+        'src/solver.f90',    # Level dasar: dipanggil oleh eigenf
+        'src/matrix.f90',    # Level dasar: dipanggil oleh eigene
+        'src/eigenf.f90',    # Level menengah: butuh solver, dipanggil oleh main
+        'src/eigene.f90',    # Level menengah: butuh matrix, dipanggil oleh main
+        'src/main.f90'       # Level puncak: butuh eigenf dan eigene
     ]
 )
 
 setup(
-    name='dvipy',
-    version='0.1.0',
-    packages=['dvipy'],
-    ext_modules=[ext],
-    install_requires=[
-        'numpy',
-    ],
+    name                =    'dvipy',
+    version             =    '0.1.0',
+    packages            =    ['dvipy'],
+    ext_modules         =    [ext],
+    install_requires    =    ['numpy'],
 )
