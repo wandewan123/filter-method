e2d03a90a82b0756e8324abbf330e7822f0b3016
Dewan Tamsih <dewanjava321@gmail.com>
2026-05-15 01:31:39 +0700

    Update utils.f90

--- src/utils.f90
+++ src/utils.f90
@@ -14,6 +14,26 @@ contains
         end if
     end function get_phi
 
+    !2. Fungsi Integral Trapezoid (Digunakan oleh eigene untuk hitung energi)
+    function trapz(y, dx) result(integral)
+        real(8), intent(in) :: y(:)
+        real(8), intent(in) :: dx
+        real(8)             :: integral
+        integer             :: n, i
+        
+        n = size(y)
+        integral = 0.0d0
+        
+        if (n > 1) then
+            ! Rumus: dx/2 * (y_1 + 2*y_2 + ... + 2*y_{n-1} + y_n)
+            integral = y(1) + y(n)
+            do i = 2, n - 1
+                integral = integral + 2.0d0 * y(i)
+            end do
+            integral = integral * (dx / 2.0d0)
+        end if
+    end function trapz
+
     ! Normalisasi Fungsi Gelombang dengan Aturan Trapezoid
     function normalize(phi, dx) result(phi_norm)
         real(8), intent(in) :: phi(:)
