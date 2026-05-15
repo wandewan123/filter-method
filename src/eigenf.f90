ce51e50447425cc448bf643bf267027bb20905f7
Dewan Tamsih <dewanjava321@gmail.com>
2026-05-15 08:21:23 +0700

    Update eigenf.f90

--- src/eigenf.f90
+++ src/eigenf.f90
@@ -16,7 +16,7 @@ subroutine eigenf2(dx, vpot, psi, n, energy, phi)
 
     allocate(alfa(n), beta(n), stat = alloc_stat)
     if (alloc_stat /= 0) then
-      print *, "ERROR: Gagal alokasi memori di eigenf4!"
+      print *, "ERROR: Gagal alokasi memori di eigenf2!"
       return
     end if
 
@@ -38,12 +38,15 @@ subroutine eigenf4(dx, vpot, psi, n, energy, phi)
     real(8), intent(out) :: phi(n)
 
     real(8)              :: dx2
+    integer              :: alloc_stat
     real(8), allocatable :: a(:), b(:), c(:), d(:), e(:)
 
     allocate(a(n), b(n), c(n), d(n), e(n), stat = alloc_stat)
-
+    if (alloc_stat /= 0) then
+      print *, "ERROR: Gagal alokasi memori di eigenf4!"
+      return
+    end if
     dx2 = dx * dx
-
     a = 1.0d0 / (24.0d0 * dx2)
     b = -2.0d0 / (3.0d0 * dx2)
     d = -2.0d0 / (3.0d0 * dx2)
@@ -58,17 +61,20 @@ subroutine eigenf4(dx, vpot, psi, n, energy, phi)
 
 end subroutine eigenf4
 
-
 subroutine eigenf6(dx, vpot, psi, n, energy, phi)
     integer, intent(in)  :: n
     real(8), intent(in)  :: dx, energy, vpot(n), psi(n)
     real(8), intent(out) :: phi(n)
 
     real(8)              :: dx2
+    integer              :: alloc_stat
     real(8), allocatable :: a3(:), a2(:), a1(:), d0(:), c1(:), c2(:), c3(:)
 
-    allocate(a3(n), a2(n), a1(n), d0(n), c1(n), c2(n), c3(n))
-
+    allocate(a3(n), a2(n), a1(n), d0(n), c1(n), c2(n), c3(n), stat = alloc_stat)
+    if (alloc_stat /= 0) then
+      print *, "ERROR: Gagal alokasi memori di eigenf6!"
+      return
+    end if
     dx2 = dx * dx
 
     a3 = -1.0d0 / (180.0d0 * dx2)
