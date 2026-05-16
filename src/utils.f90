71dc3b1a740416b0fee7130d0db6ac2555543fc7
Dewan Tamsih <dewanjava321@gmail.com>
2026-05-16 09:03:23 +0700

    Update utils.f90

--- src/utils.f90
+++ src/utils.f90
@@ -8,24 +8,22 @@ contains
         real(8) :: val
 
         if (i < 1 .or. i > n) then
-            val = 0.0d0   ! Dirichlet ghost cell
+            val = 0.0d0
         else
             val = phi(i)
         end if
     end function get_phi
 
-    !2. Fungsi Integral Trapezoid (Digunakan oleh eigene untuk hitung energi)
     function trapz(y, dx) result(integral)
         real(8), intent(in) :: y(:)
         real(8), intent(in) :: dx
         real(8)             :: integral
         integer             :: n, i
         
-        n = size(y)
+        n        = size(y)
         integral = 0.0d0
         
         if (n > 1) then
-            ! Rumus: dx/2 * (y_1 + 2*y_2 + ... + 2*y_{n-1} + y_n)
             integral = y(1) + y(n)
             do i = 2, n - 1
                 integral = integral + 2.0d0 * y(i)
