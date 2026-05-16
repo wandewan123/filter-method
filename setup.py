23fb7703cb361629b639d55c7780352140a39fb0
Dewan Tamsih <dewanjava321@gmail.com>
2026-05-16 08:58:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from numpy.distutils.core import Extension, setup
 ext = Extension(
     name       =    'dvipy.solver_lib', 
     sources    =    [
-        'src/solver.f90',    # Level dasar: dipanggil oleh eigenf
+        'src/solver.f90',
         'src/utils.f90',
-        'src/matrix.f90',    # Level dasar: dipanggil oleh eigene
-        'src/eigenf.f90',    # Level menengah: butuh solver, dipanggil oleh main
-        'src/eigene.f90',    # Level menengah: butuh matrix, dipanggil oleh main
-        'src/main.f90'       # Level puncak: butuh eigenf dan eigene
+        'src/matrix.f90',
+        'src/eigenf.f90',
+        'src/eigene.f90',
+        'src/main.f90'
     ]
 )
 
